f96e5717631ec02eacb97db149966591c91b5e5a
zainExanadu <zaynsaeed365@gmail.com>
2025-07-10 15:08:42 +0500

    Refactor Product model and product routes for improved readability and consistency

--- models/Product.js
+++ models/Product.js
@@ -1,27 +1,39 @@
-const mongoose = require('mongoose');
-const ProductSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "can't be blank"]
-  },
-  description: {
-    type: String,
-    required: [true, "can't be blank"]
-  },
-  price: {
-    type: String,
-    required: [true, "can't be blank"]
-  },
-  category: {
-    type: String,
-    required: [true, "can't be blank"]
-  },
-  pictures: {
-    type: Array,
-    required: true
-  }
-}, {minimize: false});
+const mongoose = require("mongoose");
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    description: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    price: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    category: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
 
-const Product = mongoose.model('Product', ProductSchema);
+    age: {
+      type: Array,
+      required: [false, "can be blank"],
+    },
+    sizes: {
+      type: Array,
+      required: [false, "can be blank"],
+    },
+    pictures: {
+      type: Array,
+      required: true,
+    },
+  },
+  { minimize: false }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
 
 module.exports = Product;
