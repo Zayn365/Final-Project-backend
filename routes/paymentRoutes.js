e7fb90a20cc8f693d68b1be01575d1ac1c65f190
Zayn365 <zaynsaeed365@gmail.com>
2025-08-04 20:33:33 +0500

    update: paymentRoute added for return

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -7,4 +7,13 @@ route.post("/link", (req, res) => payment.payByLink(req, res));
 route.post("/3d-session", (req, res) =>
   payment.generate3DSecureSession(req, res)
 );
+// routes/payment.js
+route.post("/result", (req, res) => {
+  const paymentResult = req.body;
+  const redirectUrl = `https://store.bikev.k12.tr/payment/result?status=${
+    paymentResult.status
+  }&message=${paymentResult.responseMsg || "OK"}`;
+  res.redirect(302, redirectUrl);
+});
+
 module.exports = route;
